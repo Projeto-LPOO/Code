e960057145c11d8140d73fa49f9a0195567cc34d
Erick Sanchez <flealafonso@gmail.com>
2026-04-30 22:42:13 -0300

    rename controller

--- src/main/java/com/aura/availability/controllers/AvailabilityController.java
+++ src/main/java/com/aura/availability/controllers/AvailabilityController.java
@@ -20,7 +20,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/autenticado/availability")
-public class AvailabilityServlet extends BaseController {
+public class AvailabilityController extends BaseController {
     private static final long serialVersionUID = 1L;
     private AvailabilityDao availabilityDao = new AvailabilityDao();
 
