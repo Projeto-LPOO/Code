78966c5827a9bb51bccae47835a1ba2c6c2537a0
Daniel Oliveira <dansoliveira.ba@gmail.com>
2026-05-16 09:47:38 -0300

    removendo antiguidades

--- src/main/java/com/aura/meeting/view/test.java
+++
@@ -1,8 +0,0 @@
-//package com.aura.meeting.view;
-//
-//public class test {
-//    public static void main(String[] args) {
-//        MeetingView view = new MeetingView();
-//        view.showMenu();
-//    }
-//}
